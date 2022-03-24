76e2e6e536e4c2a66cbaa2660790035fe2bb87eb
Chris Massie <chris@massie.scot>
2022-03-24 20:22:27 +0000

    Got rid of log message.
    Got rid of log message.

--- src/main/java/scot/massie/mc/itemcopy/Sharer.java
+++ src/main/java/scot/massie/mc/itemcopy/Sharer.java
@@ -478,8 +478,6 @@ public final class Sharer
 
     public static void purgeOldOffers()
     {
-        System.out.println("Purging old offers!");
-
         synchronized(offers)
         {
             long currentTime = System.currentTimeMillis();
